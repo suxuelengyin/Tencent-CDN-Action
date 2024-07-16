1b37a455f75c532835b9c8a9362ec43137143c89
suxue-mac <suxuewb@gmail.com>
2024-07-16 17:37:52 +0800

    first

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,50 @@
 import * as core from '@actions/core'
 import { wait } from './wait'
+import { InputParams } from './params'
+import CDN_SDK from 'tencentcloud-sdk-nodejs/tencentcloud/services/cdn'
+import { ClientConfig } from 'tencentcloud-sdk-nodejs/tencentcloud/common/interface'
 
+const CDN_CLIENT = CDN_SDK.cdn.v20180606.Client
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-
+    const secret_id: string = core.getInput(InputParams.SecretID)
+    const secret_key: string = core.getInput(InputParams.SecretKey)
+    const urls: string = core.getInput(InputParams.URLS)
+    const paths: string = core.getInput(InputParams.PATHS)
+    const clientConfig: ClientConfig = {
+      credential: {
+        secretId: secret_id,
+        secretKey: secret_key
+      },
+      profile: {
+        language: 'en-US'
+      }
+    }
+    const cdnClient = new CDN_CLIENT(clientConfig)
+    if (urls) {
+      const urlsArr = urls.split(',')
+      const urlsParams = {
+        Urls: urlsArr
+      }
+      await cdnClient.PurgeUrlsCache(urlsParams)
+    }
+    if (paths) {
+      const pathsArr = paths.split(',')
+      const pathsParams = {
+        FlushType: 'delete',
+        Paths: pathsArr
+      }
+      await cdnClient.PurgePathCache(pathsParams)
+    }
     // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    // core.debug(`Waiting ${ms} milliseconds ...`)
 
     // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('time', 'success')
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
---
+++ src/params.ts
@@ -0,0 +1,6 @@
+export enum InputParams {
+  SecretID = 'secret_id',
+  SecretKey = 'secret_key',
+  URLS = 'urls',
+  PATHS = 'paths'
+}
